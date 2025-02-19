f5c835fb74636c95f02facd6b8fd78486eaa98f3
Pratik Raje <105652654+pratik247-02@users.noreply.github.com>
2025-02-19 22:47:53 +0530

    Made a Custom Hook useCurrInfo

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-gray-700 text-white text-3xl'>Currency Convertor</h1>
+    </>
+  )
+}
+
+export default App
